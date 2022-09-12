6ef168fb0a29154497998649aef1550ed357e18f
faraz.golshahi8151 <faraz.golshahi8151@gmail.com>
2022-09-12 12:29:54 +0430

    add 140033.cpp

--- Contest/140033 - صدادار - C++/140033.cpp
+++ Contest/140033 - صدادار - C++/140033.cpp
@@ -3,24 +3,24 @@
 // Contact: info@farazgolshahi.com
 
 #include <iostream>
+
 using namespace std;
 
-int main()
-{
+int main() {
     char strng[100];  // declaring variables..
-    int i,vow=0;
-    cin>>strng; // taking string from user.
+    int i, vow = 0;
+    cin >> strng; // taking string from user.
 
     //Initializing for loop.
-    for(i=0;strng[i];i++)  // iterate loop
+    for (i = 0; strng[i]; i++)  // iterate loop
     {
         //Counting the vowels in a string.
-        if(strng[i]=='a'|| strng[i]=='e'||strng[i]=='i'||strng[i]=='o'||strng[i]=='u'||strng[i]=='A'||strng[i]=='E'||strng[i]=='I'||strng[i]=='O' ||strng[i]=='U')
-        {
+        if (strng[i] == 'a' || strng[i] == 'e' || strng[i] == 'i' || strng[i] == 'o' || strng[i] == 'u' ||
+            strng[i] == 'A' || strng[i] == 'E' || strng[i] == 'I' || strng[i] == 'O' || strng[i] == 'U') {
             vow++;
         }
     }
-    cout<<vow;
+    cout << vow;
     return 0;
 }
 
