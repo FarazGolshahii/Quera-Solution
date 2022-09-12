9089f43f849ff2816a9145b685477c5cc47b2a5c
faraz.golshahi8151 <faraz.golshahi8151@gmail.com>
2022-09-12 12:29:39 +0430

    add 72876.cpp

---
+++ Contest/72876 - غُل‌بانو و دوقلوهایش - C++/72876.cpp
@@ -0,0 +1,14 @@
+// In the name of GOD
+// Created by: Faraz Golshahi
+// Contact: info@farazgolshahi.com
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    cout << s[4] << s[3] << s[2] << s[1] << s[0];
+    return 0;
+}
