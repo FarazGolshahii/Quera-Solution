00079d76b65e61eebf6e0afffa8eeea35aec4cbe
faraz.golshahi8151 <faraz.golshahi8151@gmail.com>
2022-09-12 12:29:06 +0430

    add 6087.cpp

--- Contest/6087 - سفر بیا - C++/6087.cpp
+++ Contest/6087 - سفر بیا - C++/6087.cpp
@@ -15,9 +15,15 @@
 #include <cmath>
 #include <set>
 #include <map>
+
 using namespace std;
 
-inline int in() { int x; scanf("%d", &x); return x; }
+inline int in() {
+    int x;
+    scanf("%d", &x);
+    return x;
+}
+
 const int N = 2112, E = 8 * N, INF = 1e9;
 
 int head[N], from[E], to[E], bro[E], nex[E], cap[E], w[E], cntE;
@@ -25,8 +31,7 @@ int par[N], d[N], have[N];
 bool inQ[N];
 bool isGood[N];
 
-void _addE(int u, int v, int c, int weight)
-{
+void _addE(int u, int v, int c, int weight) {
     int e = cntE++;
     bro[e] = head[u];
     head[u] = e;
@@ -36,44 +41,39 @@ void _addE(int u, int v, int c, int weight)
     to[e] = v;
 }
 
-void addE(int u, int v, int c, int weight)
-{
+void addE(int u, int v, int c, int weight) {
     _addE(u, v, c, weight);
     _addE(v, u, 0, -weight);
 }
 
 int in(int v) { return v * 2; }
+
 int out(int v) { return v * 2 + 1; }
 
 void addE(int u, int v, int w) { addE(out(u), in(v), 1, w); }
 
-void push(queue <int> &q, int v)
-{
-    if(inQ[v])
+void push(queue<int> &q, int v) {
+    if (inQ[v])
         return;
     inQ[v] = true;
     q.push(v);
 }
 
-void push(int s, int t)
-{
+void push(int s, int t) {
     fill(d, d + N, INF);
     fill(par, par + N, -1);
     fill(have, have + N, 0);
     have[s] = 1;
-    queue <int> q;
+    queue<int> q;
     d[s] = 0;
     push(q, s);
-    while(q.size())
-    {
+    while (q.size()) {
         int v = q.front();
         q.pop();
         inQ[v] = false;
-        for(int e = head[v]; e != -1; e = bro[e])
-        {
+        for (int e = head[v]; e != -1; e = bro[e]) {
             int u = to[e];
-            if(cap[e] > 0 && d[u] > d[v] + w[e])
-            {
+            if (cap[e] > 0 && d[u] > d[v] + w[e]) {
                 par[u] = e;
                 d[u] = d[v] + w[e];
                 have[u] = min(have[v], cap[e]);
@@ -83,18 +83,15 @@ void push(int s, int t)
     }
 }
 
-int minCostMaxFlow(int s, int t, int &flow)
-{
+int minCostMaxFlow(int s, int t, int &flow) {
     int cost = 0;
     flow = 0;
-    while(1)
-    {
+    while (1) {
         push(s, t);
-        if(par[t] == -1)
+        if (par[t] == -1)
             break;
         int f = have[t];
-        for(int e = par[t]; e != -1; e = par[from[e]])
-        {
+        for (int e = par[t]; e != -1; e = par[from[e]]) {
             cost += f * w[e];
             cap[e] -= f;
             cap[e ^ 1] += f;
@@ -104,23 +101,21 @@ int minCostMaxFlow(int s, int t, int &flow)
     return cost;
 }
 
-int main()
-{
+int main() {
     int n = in(), m = in(), k = in();
     string mp;
     cin >> mp;
     fill(head, head + N, -1);
     int nGoods = 0;
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
         nGoods += (isGood[i] = (mp[i] == '1'));
-    while(m--)
-    {
+    while (m--) {
         int u = in() - 1, v = in() - 1, w = in();
         assert(w != 0);
         addE(u, v, w);
         addE(v, u, w);
     }
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
         addE(in(i), out(i), 1, 0);
     push(in(0), out(n - 1));
     int ans = d[in(n - 1)];
@@ -128,17 +123,16 @@ int main()
     int s = 2 * n, t = 2 * n + 1;
     addE(out(0), t, 1, 0);
     addE(out(n - 1), t, 1, 0);
-    for(int i = 0; i < n; i++)
-        if(isGood[i])
+    for (int i = 0; i < n; i++)
+        if (isGood[i])
             addE(s, in(i), 1, 0);
     int flow;
     int cost = minCostMaxFlow(s, t, flow);
-    if(flow == 2)
-    {
+    if (flow == 2) {
         ans = min(ans, cost + k);
         ans = min(ans, cost + k * (nGoods - 1));
     }
-    if(ans >= INF)
+    if (ans >= INF)
         cout << "Impossible\n";
     else
         cout << ans << endl;
