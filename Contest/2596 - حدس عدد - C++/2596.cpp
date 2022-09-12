abbf4e2cf0a2c69202b23d61d229a4e455ffaaaa
faraz.golshahi8151 <faraz.golshahi8151@gmail.com>
2022-09-12 12:07:23 +0430

    add 2596

---
+++ Contest/2596 - حدس عدد - C++/2596.cpp
@@ -0,0 +1,29 @@
+// In the name of GOD
+// Created by: Faraz Golshahi
+// contact: info@farazgolshahi.com
+
+#include <bits/stdc++.h>
+using namespace std;
+
+set<int> a;
+
+bool isOk(int n) {
+    set<int> b = a;
+    for(int i = 1; i <= n; i++)
+        if(n % i == 0)
+            b.erase(i);
+    return b.empty();
+}
+
+int main(){
+    int q, x;
+    cin>>q;
+    for(int i = 0; i < q; i++) {
+        cin>>x;
+        a.insert(x);
+    }
+    int ans = 0;
+    for(int i = 1; i <= 1000; i++)
+        ans += isOk(i);
+    cout<<ans;
+}
