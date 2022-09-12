0fdccc885b8d23e9b27bd11dffbd81efdeb69d95
faraz.golshahi8151 <faraz.golshahi8151@gmail.com>
2022-09-12 13:57:41 +0430

    add 72877.cpp

---
+++ Contest/72877 - پشمال‌آقا - C++/72877.cpp
@@ -0,0 +1,15 @@
+// In the name of GOD
+// Created by: Faraz Golshahi
+// Contact: info@farazgolshahi.com
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    float a, b, c;
+    cin >> a >> b >> c;
+    cout << a * b * c;
+    return 0;
+}
+
