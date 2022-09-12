3437e70fdac6e2aa3870b882663393a7a6eeb998
faraz.golshahi8151 <faraz.golshahi8151@gmail.com>
2022-09-12 12:29:27 +0430

    add 15127.cpp

--- Contest/15127 - صمد و توک - C++/15127.cpp
+++ Contest/15127 - صمد و توک - C++/15127.cpp
@@ -13,31 +13,31 @@ const int N = 510;
 
 int dis[N][N];
 
-int main()
-{
-    ios_base::sync_with_stdio(0); cin.tie(0);
-    int n; cin >> n;
-    for(int i=0; i<n; i++)
-        for(int j=0; j<n; j++)
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
             cin >> dis[i][j];
 
-    for(int i=0; i<n; i++)
-        if(dis[i][i] != 0)return 0;
-    for(int i=0; i<n; i++)
-        for(int j=0; j<n; j++)
-            if(dis[i][j] != dis[j][i])return 0;
+    for (int i = 0; i < n; i++)
+        if (dis[i][i] != 0)return 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            if (dis[i][j] != dis[j][i])return 0;
 
     int ans = 0;
 
-    for(int i=0; i<n; i++)
-        for(int j=i+1; j<n; j++){
+    for (int i = 0; i < n; i++)
+        for (int j = i + 1; j < n; j++) {
             bool ok = false;
-            for(int k=0; k<n; k++)
-            {
-                if(dis[i][j] > dis[i][k] + dis[k][j])return cout << -1 << endl, 0;
-                else if(k != i && k != j && dis[i][j] == dis[i][k] + dis[k][j])ok = true;
+            for (int k = 0; k < n; k++) {
+                if (dis[i][j] > dis[i][k] + dis[k][j])return cout << -1 << endl, 0;
+                else if (k != i && k != j && dis[i][j] == dis[i][k] + dis[k][j])ok = true;
             }
-            if(!ok)ans ++;
+            if (!ok)ans++;
         }
     cout << ans << endl;
 }
