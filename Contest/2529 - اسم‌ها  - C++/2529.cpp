5ff31538e87313806ad554bbaabccfeeb5f2df26
faraz.golshahi8151 <faraz.golshahi8151@gmail.com>
2022-09-12 13:55:54 +0430

    add 2529.cpp

---
+++ Contest/2529 - اسم‌ها  - C++/2529.cpp
@@ -0,0 +1,49 @@
+// In the name of GOD
+// Created by: Faraz Golshahi
+// Contact: info@farazgolshahi.com
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long gcd(long long int a, long long int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+long long lcm(int a, int b) {
+    return (a / gcd(a, b)) * b;
+}
+
+typedef vector<int> vi;
+typedef long long ll;
+
+#define F first
+#define S second
+#define pb push_back
+#define endl '\n'
+#define all(o) o.begin(), o.end()
+#define fori(i, a) for (int i = 0; i < (a); i++)
+const int MAXN = 4e5 + 10;
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    /////////////////////////////////////////////////////////////
+    int n, j, i, s1 = 0, s2 = 0;
+    string s;
+    cin >> n;
+    fori(j, n) {
+        cin >> s;
+        sort(s.begin(), s.end());
+        s1 = 0;
+        fori(i, s.size() - 1) {
+            if (s[i] != s[i + 1])s1++;
+        }
+        s1++;
+        s2 = max(s2, s1);
+    }
+    cout << s2;
+    /////////////////////////////////////////////////////////////
+    return 0;
+}
