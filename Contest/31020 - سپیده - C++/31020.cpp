e3fffd5521e61607df4144940e9af7fd7cd40ffb
faraz.golshahi8151 <faraz.golshahi8151@gmail.com>
2022-09-12 14:32:22 +0430

    add 31020.cpp

---
+++ Contest/31020 - سپیده - C++/31020.cpp
@@ -0,0 +1,29 @@
+// In the name of GOD
+// Created by: Faraz Golshahi
+// Contact: info@farazgolshahi.com
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long ll;
+
+#define F first
+#define S second
+#define pb push_back
+#define endl '\n'
+#define all(o) o.begin(), o.end()
+#define fori(i, a) for (int i = 0; i < (a); i++)
+const int MAXN = 4e5 + 10;
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    /////////////////////////////////////////////////////////////
+    float n, m, c;
+    cin >> m >> n;
+    c = m / n;
+    cout << ceil(c);
+    /////////////////////////////////////////////////////////////
+    return 0;
+}
