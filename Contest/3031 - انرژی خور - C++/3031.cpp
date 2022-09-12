2ec70c26fe37e22c51d5f3c42818d4390caa618b
faraz.golshahi8151 <faraz.golshahi8151@gmail.com>
2022-09-12 13:56:08 +0430

    add 3031.cpp

---
+++ Contest/3031 - انرژی خور - C++/3031.cpp
@@ -0,0 +1,26 @@
+// In the name of GOD
+// Created by: Faraz Golshahi
+// Contact: info@farazgolshahi.com
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    long long n, k;
+    cin >> n >> k;
+    pair<int, int> a[n];
+    for (int i = 0; i < n; i++) cin >> a[i].first >> a[i].second;
+    sort(a, a + n);
+
+    for (int i = 0; i < n; i++) {
+        if (k >= a[i].first && a[i].first < a[i].second) {
+            k -= a[i].first;
+            k += a[i].second;
+        }
+    }
+
+    cout << k << endl;
+
+    return 0;
+}
