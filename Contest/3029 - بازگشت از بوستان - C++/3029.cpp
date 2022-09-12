eebddb0975d1e6246eecbc8c7f59616bd1d5bdae
faraz.golshahi8151 <faraz.golshahi8151@gmail.com>
2022-09-12 15:00:31 +0430

    add 3029.cpp

---
+++ Contest/3029 - بازگشت از بوستان - C++/3029.cpp
@@ -0,0 +1,16 @@
+// In the name of GOD
+// Created by: Faraz Golshahi
+// Contact: info@farazgolshahi.com
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int x1, x2, y1, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    if (x1 < x2)
+        cout << "Right" << endl;
+    else
+        cout << "Left" << endl;
+}
