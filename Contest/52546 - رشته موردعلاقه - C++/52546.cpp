9db93622bc406115ddfbdce7cb266343be831937
faraz.golshahi8151 <faraz.golshahi8151@gmail.com>
2022-09-12 13:57:20 +0430

    add 52546.cpp

---
+++ Contest/52546 - رشته موردعلاقه - C++/52546.cpp
@@ -0,0 +1,40 @@
+// In the name of GOD
+// Created by: Faraz Golshahi
+// Contact: info@farazgolshahi.com
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string a;
+    cin >> a;
+    int alen = a.length();
+
+    int n;
+    cin >> n;
+
+    string rs[n];
+    for (int i = 0; i < n; i++)
+        cin >> rs[i];
+
+    int tedad = 0;
+    for (int i = 0; i < n; i++) {
+        int slen = rs[i].length();
+        bool e = false, shamel = false;
+        int idx = 0;
+        for (int j = 0; j < slen; j++) {
+            if (rs[i][j] == a[idx]) {
+                idx++;
+            }
+
+            if (idx == alen) {
+                tedad++;
+                break;
+            }
+        }
+    }
+
+    cout << tedad;
+}
