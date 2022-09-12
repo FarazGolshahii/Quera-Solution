3c8c249929d3bfcf3d06482c7eaa94a475a80d6a
faraz.golshahi8151 <faraz.golshahi8151@gmail.com>
2022-09-12 15:01:03 +0430

    add 3537.cpp

---
+++ Contest/3537 - سوال زرد - C++/3537.cpp
@@ -0,0 +1,18 @@
+// In the name of GOD
+// Created by: Faraz Golshahi
+// Contact: info@farazgolshahi.com
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, i;
+    cin >> n;
+    cout << "W";
+    for (i = 0; i < n; i++) {
+        cout << "o";
+    }
+    cout << "w!";
+    return 0;
+}
