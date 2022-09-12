38f8bb73e83eb932dc6b63d91fde146d2c3b6e9f
faraz.golshahi8151 <faraz.golshahi8151@gmail.com>
2022-09-12 15:02:20 +0430

    add 144082.cpp

---
+++ Contest/144082 - کیک در کوئرا - C++/144082.cpp
@@ -0,0 +1,15 @@
+// In the name of GOD
+// Created by: Faraz Golshahi
+// Contact: info@farazgolshahi.com
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (12 % n == 0)cout << "YES";
+    else cout << "NO";
+    return 0;
+}
