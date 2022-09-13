08f4f478267ad6d4e351251ecf9bbb107d6cfccc
faraz.golshahi8151 <faraz.golshahi8151@gmail.com>
2022-09-13 09:53:30 +0430

    add 33026.py

---
+++ Contest/33026 - چی‌سون؟ - Python3.8/33026.py
@@ -0,0 +1,18 @@
+# In the name of GOD
+# Created by: Faraz Golshahi
+# Contact: info@farazgolshahi.com
+
+import json
+
+n = int(input())
+l = {}
+for i in range(n):
+    s = input()
+    if s.startswith("print"):
+        nameidx = s.split(" ")[1].split("[")
+        if nameidx[1][:-1].startswith("\""):
+            print(l[nameidx[0]][nameidx[1][1:-2]])
+        else:
+            print(l[nameidx[0]][int(nameidx[1][:-1])])
+    elif s.split(" ")[1] == ':=':
+        l.update({s.split(" ")[0]: json.loads(s[len(s.split(" ")[0])+4:])})
