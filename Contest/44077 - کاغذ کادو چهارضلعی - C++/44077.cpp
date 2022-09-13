92ad63a3f01bbc4ad314738e50145a5626d8ce98
faraz.golshahi8151 <faraz.golshahi8151@gmail.com>
2022-09-13 09:53:39 +0430

    add 44077.cpp

---
+++ Contest/44077 - کاغذ کادو چهارضلعی - C++/44077.cpp
@@ -0,0 +1,22 @@
+// In the name of GOD
+// Created by: Faraz Golshahi
+// Contact: info@farazgolshahi.com
+
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long double a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    long double s = (a + b + c + d) / 2;
+    cout << setprecision(12) << sqrt((s - a) * (s - b) * (s - c) * (s - d));
+
+    return 0;
+}
